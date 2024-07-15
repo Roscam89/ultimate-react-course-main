1c243c51af03a894193cae1e3654e5b0f0ddad9c
Roscam89 <83566347+Roscam89@users.noreply.github.com>
2024-07-15 16:44:06 -0700

    modified:   04-steps/starter/steps/src/App.js

--- 04-steps/starter/steps/src/App.js
+++ 04-steps/starter/steps/src/App.js
@@ -26,10 +26,10 @@ export default function App() {
 
       {isOpen && (
         <div className="steps">
-          <div className="numbers">
-            <div className={`${step === 1 ? "active" : ""}`}>1</div>
-            <div className={`${step === 2 ? "active" : ""}`}>2</div>
-            <div className={`${step === 3 ? "active" : ""}`}>3</div>
+          <div className={`numbers`}>
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
           <p className="message">
@@ -37,21 +37,28 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>⬅️</span> Previous
+            </Button>
+
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
               Next
-            </button>
+              <span>➡️</span>
+            </Button>
           </div>
         </div>
       )}
     </>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
